a6fcd73a66453c6a4e3fc5fb049270a52fa4832e
Farez Prieto <kyo20052@gmail.com>
2018-08-09 16:13:07 -0500

    Ajuste del archivo config.php

--- config.php
+++ config.php
@@ -1,20 +1,20 @@
 <?php 
 //DATOS REALES
-/*define("_LINK_RESP","https://www.acdac.org/donaciones/respuesta-pago");
+define("_LINK_RESP","https://www.acdac.org/donaciones/respuesta-pago");
 define("_LINK_CONFIRM","https://www.acdac.org/donaciones/confirmacion-pago");
 define("_API_KEY","J7VVFpYpHz24sz93geAkQZ92NI");
 define("_REFERENCE","Donacion ACDAC - ".strtotime(date("Y-m-d H:i:s")));
 define("_MERCHANT_ID","740463");
-define("_ACCOUNT_ID","746034");*/
+define("_ACCOUNT_ID","746034");
 
 //DATOS DE PRUEBA
-define("_LINK_RESP","https://www.acdac.org/donaciones/respuesta-pago");
+/*define("_LINK_RESP","https://www.acdac.org/donaciones/respuesta-pago");
 define("_LINK_CONFIRM","https://www.acdac.org/donaciones/confirmacion-pago");
 define("_API_KEY","6benk3fl6ea8v6h8cgiiobhid9");
 define("_REFERENCE","Donacion ACDAC - ".strtotime(date("Y-m-d H:i:s")));
 define("_MERCHANT_ID","504715");
 define("_ACCOUNT_ID","505675");
-
+*/
 
 define('_HOST'											,'localhost');
 //Usuario base de datos
@@ -23,23 +23,4 @@ define('_USER'											,'acdacorg_lanUser');
 define('_PASS'											,'Jg$E3D+u');
 //base de datos
 define('_DB'											,'acdacorg_landing');
-
-?>
-
-<?php 
-/*define("_LINK_RESP","https://www.acdac.org/donaciones/respuesta-pago");
-define("_LINK_CONFIRM","https://www.acdac.org/donaciones/confirmacion-pago");
-define("_API_KEY","6benk3fl6ea8v6h8cgiiobhid9");
-define("_REFERENCE","Donacion ACDAC - ".rand(1,1000000));
-define("_MERCHANT_ID","504715");
-define("_ACCOUNT_ID","505675");
-
-
-define('_HOST'											,'localhost');
-//Usuario base de datos
-define('_USER'											,'acdacorg_lanUser');
-//Contraseña del usuario
-define('_PASS'											,'Jg$E3D+u');
-//base de datos
-define('_DB'											,'acdacorg_landing');*/
 ?>
